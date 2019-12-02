8cd9ab1f7e2e66800294c5a107c1e8a5863f97df
PyeongGangKim <21500185@handong.com>
2019-12-03 00:56:48 +0900

    make two hash table

--- example/count.c
+++ example/count.c
@@ -14,7 +14,6 @@ print_counter (gpointer key, gpointer value, gpointer userdata)
 		printf("(%s, %d)\n", t, *d) ;
 	}
 }
-
 gboolean remove_counter(gpointer key, gpointer value, gpointer user_data){
 	char * t = key;
 	int * d = value;
@@ -30,17 +29,17 @@ gboolean remove_counter(gpointer key, gpointer value, gpointer user_data){
 int 
 main () 
 {
-	FILE * f = fopen("../data/train.negative.csv", "r") ;
-
-	GHashTable * counter = g_hash_table_new(g_str_hash, g_str_equal) ;		
+	FILE * nf = fopen("../data/train.negative.csv", "r") ;
+	FILE * pf = fopen("../data/train.non-negative.csv", "r");
+	
+	GHashTable * nCounter = g_hash_table_new(g_str_hash, g_str_equal) ;		
 	
 	struct sb_stemmer * stemmer;
 	stemmer = sb_stemmer_new("english", 0x0);
 	char * line = 0x0 ;
-	size_t r ; 
 	size_t n = 0 ;
 
-	while (getline(&line, &n, f) >= 0) {
+	while (getline(&line, &n, nf) >= 0) {
 		char * t ;
 		char * _line = line ;
 		for (t = strtok(line, " \n\t") ; t != 0x0 ; t = strtok(0x0, " \n\t")) {
@@ -62,11 +61,11 @@ main ()
 			const char * s;
 			s = sb_stemmer_stem(stemmer,t,strlen(t)) ;
 			int * d ;			
-			d = g_hash_table_lookup(counter, s) ;
+			d = g_hash_table_lookup(nCounter, s) ;
 			if (d == NULL) {
 				d = malloc(sizeof(int)) ;
 				*d = 1 ;
-				g_hash_table_insert(counter, strdup(s), d) ;
+				g_hash_table_insert(nCounter, strdup(s), d) ;
 			}
 			else {
 				*d = *d + 1 ;
@@ -75,9 +74,10 @@ main ()
 		free(_line) ;
 		line = 0x0 ;
 	}
-	g_hash_table_foreach_remove(counter,remove_counter,0x0);
-	g_hash_table_foreach(counter, print_counter, 0x0) ;
-	GList * list = g_hash_table_get_values(counter);
+
+	g_hash_table_foreach_remove(nCounter,remove_counter,0x0);
+//	g_hash_table_foreach(nCounter, print_counter, 0x0) ;
+	GList * list = g_hash_table_get_values(nCounter);
 	int total;
 	total=0;
 	int i=0;
@@ -87,8 +87,64 @@ main ()
 		i++;
 		total+=*temp;
 	}
-	printf("%d\n",total);
-	printf("last: %d\n", *((int *) g_hash_table_lookup(counter, "last"))) ;
+//	printf("%d\n",total);
+//	printf("bad: %d\n", *((int *) g_hash_table_lookup(nCounter, "bad"))) ;
+	fclose(nf);
 
-	fclose(f) ;
+	GHashTable * pCounter = g_hash_table_new(g_str_hash, g_str_equal) ;		
+	
+	char * pline = 0x0 ;
+	size_t pn = 0 ;
+
+	while (getline(&pline, &pn, pf) >= 0) {
+		char * t ;
+		char * _line = pline ;
+		for (t = strtok(pline, " \n\t") ; t != 0x0 ; t = strtok(0x0, " \n\t")) {
+			int i=0;
+			while(t[i]!=0x0){
+				if(!(t[i]>='a'&&t[i]<='z')){
+					if(t[i]>='A'&&t[i]<='Z'){
+						t[i]=t[i]+32;
+					}
+					else{
+						for(int tempNum=i; t[tempNum]!=0x0; tempNum++){
+							t[tempNum]=t[tempNum+1];
+						}
+						i--;
+					}
+				}
+				i++;	
+			}
+			const char * s;
+			s = sb_stemmer_stem(stemmer,t,strlen(t)) ;
+			int * d ;			
+			d = g_hash_table_lookup(pCounter, s) ;
+			if (d == NULL) {
+				d = malloc(sizeof(int)) ;
+				*d = 1 ;
+				g_hash_table_insert(pCounter, strdup(s), d) ;
+			}
+			else {
+				*d = *d + 1 ;
+			}
+		}
+		free(_line) ;
+		pline = 0x0 ;
+	}
+
+	g_hash_table_foreach_remove(pCounter,remove_counter,0x0);
+//	g_hash_table_foreach(pCounter, print_counter, 0x0) ;
+	GList * plist = g_hash_table_get_values(pCounter);
+	int ptotal;
+	ptotal=0;
+	int pi=0;
+	while(1){
+		int *temp = g_list_nth_data(plist,pi);
+		if(temp==NULL) break;
+		pi++;
+		ptotal+=*temp;
+	}
+//	printf("%d\n",ptotal);
+//	printf("bad: %d\n", *((int *) g_hash_table_lookup(pCounter, "bad"))) ;
+	fclose(pf);
 }
