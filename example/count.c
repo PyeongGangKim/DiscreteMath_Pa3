6bdda3969756d4686bfad65565fb521413cfd0ea
KylieKim95 <21700163@handong.edu>
2019-12-04 11:53:58 +0900

    everything up-to-date

--- example/count.c
+++ example/count.c
@@ -47,7 +47,8 @@ main ()
 {
 	FILE * nf = fopen("../data/train.negative.csv", "r") ;
 	FILE * pf = fopen("../data/train.non-negative.csv", "r");
-	
+	FILE * cf = fopen("model.csv","w");
+
 	GHashTable * nCounter = g_hash_table_new(g_str_hash, g_str_equal) ;		
 	
 	struct sb_stemmer * stemmer;
@@ -90,6 +91,8 @@ main ()
 		free(_line) ;
 		line = 0x0 ;
 	}
+//Tokenization-Negative Sentences
+
 
 	g_hash_table_foreach_remove(nCounter,remove_counter,0x0);
 	GList * list = g_hash_table_get_values(nCounter);
@@ -103,6 +106,9 @@ main ()
 		total+=*temp;
 	}
 	fclose(nf);
+
+//make hash table "nCounter" that includes the appearance frequency of each word in the bag of negative words
+
 
 	GHashTable * pCounter = g_hash_table_new(g_str_hash, g_str_equal) ;		
 	
@@ -145,6 +151,9 @@ main ()
 		pline = 0x0 ;
 	}
 
+//Tokenization-Positive Sentences
+
+
 	g_hash_table_foreach_remove(pCounter,remove_counter,0x0);
 	GList * plist = g_hash_table_get_values(pCounter);
 	int ptotal;
@@ -157,6 +166,10 @@ main ()
 		ptotal+=*temp;
 	}
 	fclose(pf);
+
+//make hash table "pCounter" that includes the appearance frequency of each word in the bag of positive words
+
+
 	list=g_hash_table_get_keys(nCounter);
 	plist=g_hash_table_get_keys(pCounter);
 	GHashTable * kCounter = g_hash_table_new(g_str_hash,g_int_equal);
@@ -166,8 +179,6 @@ main ()
 		if(temp==NULL) break;
 		int * d =malloc(sizeof(int));
 		*d=i;
-	//	char * d=malloc(sizeof(char)*12);
-	//	sprintf(d,"%d",i);
 		g_hash_table_insert(kCounter,d,strdup(temp));
 		i++;
 	}
@@ -179,26 +190,31 @@ main ()
 		
 		int * d =malloc(sizeof(int));
 		*d=i;
-//		char * d=malloc(sizeof(char)*12);
-//		sprintf(d,"%d",i);
 		char * checkVal=g_hash_table_find(kCounter,find_value,temp);
 		if(checkVal==NULL) g_hash_table_insert(kCounter,d,strdup(temp));
 		i++;
 	}
-//	g_hash_table_foreach(kCounter,print_counter_string,0x0);
+//make hash table "kCounter" that includes all words in the negative and positive sentences.(kCOunter means key-Counter)
+
+
 	int num=0;
 	float nprob,pprob;
 	float prob;
-	int smoothing=10;
+	int smoothing=5;
+	int *nNum;
+	int *pNum;
+	char *temp;
+
 	GList * klist = g_hash_table_get_values(kCounter);
 	while(1){
-		char*temp=g_list_nth_data(klist,num);
+		
+		temp = g_list_nth_data(klist,num);
+
 		if(temp==NULL) break;
 
-		int *nNum;
-		int *pNum;
 		nNum=g_hash_table_lookup(nCounter,temp);
 		pNum=g_hash_table_lookup(pCounter,temp);
+
 		if(nNum==NULL){
 			nNum=malloc(sizeof(int));
 			*nNum=0;
@@ -209,10 +225,15 @@ main ()
 		}
 		pprob=(float)(*pNum+smoothing)/(ptotal+smoothing*2);
 		nprob=(float)(*nNum+smoothing)/(total+smoothing*2);
-		prob=(nprob)/(nprob+pprob);
-		printf("%d %d %d %d %f %f %f\n",*nNum,*pNum,total,ptotal,nprob,pprob,prob);
+		//prob=(nprob)/(nprob+pprob);
+		fprintf(cf,"%s,%f,%f\n",temp,nprob,pprob);
 		num++;
+
 		free(nNum);
 		free(pNum);
+		
 	}
+	fclose(cf);
+
+//make "model.csv" file. It includes 'word,negative possibility, positive possibility' info.
 }
